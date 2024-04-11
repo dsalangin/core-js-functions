100a1904693bd7ff8c65db5af702f049c7a8fca8
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-04-11 23:23:05 +0300

    feat: resolve getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,11 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.reduce((acc, func) => {
+    acc.push(func.length);
+    return acc;
+  }, []);
 }
 
 /**
